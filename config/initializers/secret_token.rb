0b3210734c281b197b4c0d478e0f1a6fcdc50f98
Go Endo <go.endo@icloud.com>
2013-07-21 14:11:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a7a697dd4aa8790f8de810dffbcfe1447e2039309415ff7e26621cd69880b5dadd1ba2428ad1ea5e20651ba153794e7683773a3d479c5a2db493e1f1e4cc8539'
